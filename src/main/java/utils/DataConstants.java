f5eda76e93f62504c993dce6a7286b3e5454ea7f
RogerTito455 <crtitofarez@gmail.com>
2026-05-06 10:37:53 +0200

    Replace Hardcoded Strings with Constants in Constants Class #2

---
+++ src/main/java/utils/DataConstants.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author crtit
+ */
+public class DataConstants {
+
+    private DataConstants() {
+    } // evita instanciación
+
+    public static final String ARRAY_LIST = "ArrayList";
+    public static final String HASH_MAP = "HashMap";
+    public static final String FILE_NORMAL = "File";
+    public static final String FILE_SERIALIZATION = "File (Serialization)";
+    public static final String SQL_DATABASE = "SQL - Database";
+    public static final String JPA = "JPA";
+}
